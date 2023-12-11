8783f23db96b340cc5d56bd73ab005f382e2fec6
srinath-T-design <srinaththiyagarajan707@gmail.com>
2023-12-11 12:57:41 +0530

    Remove unwanted code

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -23,22 +23,22 @@ public class ExExample
     }
 }
 //Console.WriteLine("Hello, World!");
-using System;
-public class ExExample
-{
-    public static void Main(string[] args)
-    {
-        try
-        {
-            int a = 10;
-            int b = 0;
-            int x = a / b;
-        }
-        catch (Exception e) 
-        { Console.WriteLine(e); }
-        finally { Console.WriteLine("Finally block is executed"); }
-        Console.WriteLine("Rest of the code");
-    }
-}
+//using System;
+//public class ExExample
+//{
+//    public static void Main(string[] args)
+//    {
+//        try
+//        {
+//            int a = 10;
+//            int b = 0;
+//            int x = a / b;
+//        }
+//        catch (Exception e) 
+//        { Console.WriteLine(e); }
+//        finally { Console.WriteLine("Finally block is executed"); }
+//        Console.WriteLine("Rest of the code");
+//    }
+//}
 
 
