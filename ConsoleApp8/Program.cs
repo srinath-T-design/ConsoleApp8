023bb654d5c2813646ae24cfcb356da44fbc04ba
srinath-T-design <srinaththiyagarajan707@gmail.com>
2023-12-11 12:53:48 +0530

    Added new consoleApp code

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -1,4 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+using System;
+using System.Threading.Channels;
+
+public class ExExample
+{
+    
+    public static void Main(string[] args)
+    {
+        try
+        {
+            int a = 10;
+            int b = 0;
+            int x = a / b;
+        }
+        catch (Exception e)
+        { Console.WriteLine(e); }
+        finally { Console.WriteLine("Finally block is executed:"); }
+        Console.WriteLine("Rest of the code");
+        Console.WriteLine("-----------------");
+    }
+}
 
 
