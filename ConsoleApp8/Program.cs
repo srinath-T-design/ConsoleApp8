110e82d7d7e5a96284dca47c209aa9a568d2f4f8
srinath-T-design <srinaththiyagarajan707@gmail.com>
2023-12-11 12:40:40 +0530

    remove one line

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hi....!");
-Console.WriteLine("test demo working fine..");
+
 
